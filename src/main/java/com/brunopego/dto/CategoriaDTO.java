e2497ead527c300492c04c74897679efd0701fd1
Bruno Lacerda <brunolcpg@gmail.com>
2019-04-14 15:38:54 -0300

    Lista categorias com DTO

---
+++ src/main/java/com/brunopego/dto/CategoriaDTO.java
@@ -0,0 +1,37 @@
+package com.brunopego.dto;
+
+import java.io.Serializable;
+
+import com.brunopego.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+
+	private Integer id;
+	private String nome;
+
+	public CategoriaDTO() {
+
+	}
+
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
