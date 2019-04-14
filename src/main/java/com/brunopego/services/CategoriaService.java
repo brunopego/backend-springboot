a26e37eb349d110c6eefcdb7b6fc61b0cc6bfaca
Bruno Lacerda <brunolcpg@gmail.com>
2019-04-14 15:22:54 -0300

    Adiciona função de delete em categoria

--- src/main/java/com/brunopego/services/CategoriaService.java
+++ src/main/java/com/brunopego/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.brunopego.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.brunopego.domain.Categoria;
 import com.brunopego.repositories.CategoriaRepository;
+import com.brunopego.services.exceptions.DataIntegrityException;
 import com.brunopego.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,6 +32,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria qeu possui produtos");
+		}
 	}
 
 }
