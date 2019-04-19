c7c9291f16d14698756442bbaebf33b9032e604b
Bruno Lacerda <brunolcpg@gmail.com>
2019-04-19 16:33:12 -0300

    Validação e função para inserir Pedido

--- src/main/java/com/brunopego/domain/PagamentoComCartao.java
+++ src/main/java/com/brunopego/domain/PagamentoComCartao.java
@@ -3,8 +3,10 @@ package com.brunopego.domain;
 import javax.persistence.Entity;
 
 import com.brunopego.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	
 	private Integer numeroDeParcelas;
