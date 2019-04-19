aaf2e3b19e4350112455e633e6f4207553cc7492
Bruno Lacerda <brunolcpg@gmail.com>
2019-04-19 12:29:30 -0300

    Validação customizada - email não repetido na atualização de cliente

--- src/main/java/com/brunopego/dto/ClienteDTO.java
+++ src/main/java/com/brunopego/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.brunopego.domain.Cliente;
+import com.brunopego.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private Integer id;
